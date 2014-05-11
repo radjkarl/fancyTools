edc920109353d146ebf7b345534a54abcd600981
radjkarl <karl@bedrich.de>
2014-05-11 23:34:28 +0100

    first_commit

--- fancytools/__init__.py
+++ fancytools/__init__.py
@@ -1,9 +1,10 @@
-__all__ = ['__version__', 'fcollections', 'math', 'os', 'pystructure', 'tests', 'utils']
-# Don't modify the line above, or this line!
-import autoxinit
-autoxinit.autoxinit(__name__, __file__, globals())
-del autoxinit
-# Anything else you want can go after here, it won't get modified.
+try:
+	__all__ = ['__version__', 'fcollections', 'math', 'os', 'pystructure', 'tests', 'utils']
+	# Don't modify the line above, or this line!
+	import autoxinit
+	autoxinit.autoxinit(__name__, __file__, globals())
+	del autoxinit
+	# Anything else you want can go after here, it won't get modified.
 except ImportError:
 	pass
 
