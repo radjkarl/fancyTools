55091868770b58cad4bcd40e0f20a9dabd93d2b0
radjkarl <karl@bedrich.de>
2014-05-13 00:40:17 +0100

    update

--- upload_pypi_github.sh
+++ upload_pypi_github.sh
@@ -19,4 +19,26 @@ git push origin master # Sends your commits in the "master" branch to GitHub
 #API on GitHub pages
 ####################
 
+sphinx-apidoc -A "Karl Bedrich" -F -f -o doc fancytools
 
+cd doc/_build
+
+git clone https://github.com/radjkarl/fancyTools.git gh-pages
+
+rm -r html
+mv gh-pages html
+cd html
+
+git rm -rf -f .
+git clean -fxd
+cd ../../
+
+
+
+make html
+cd _build/html
+git add .
+git commit -a -m 'API updated'
+git push -f origin HEAD:gh-pages 
+
+cd ../../..
