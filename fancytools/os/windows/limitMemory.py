c36d7af3733d5e6e865877787ceaa6d3373ef7f5
Karl Bedrich <karl@bedrich.de>
2017-02-28 14:10:50 +0000

    cleanup

--- fancytools/os/windows/limitMemory.py
+++ fancytools/os/windows/limitMemory.py
@@ -13,16 +13,13 @@ class _CheckMem(Thread):
         self.every = every
         self.maxMem = maxMem
 
-
     def run(self):
         while not self.stopped.wait(self.every):
             process = psutil.Process(os.getpid())
             curMem = process.memory_info().rss 
             if curMem > self.maxMem:
-                print('used to much memory: %s GB' %(curMem/8/1024**3))
+                print('Used to much memory: %s GB' %(curMem/1024**3))
                 os._exit(1)
-
-
 
 
 class limitMemory(object):
@@ -31,18 +28,18 @@ class limitMemory(object):
         maxMemory [GB]
             not not set, set to 50% of avail memory
         every [sec]
-        This function starts a subprocess
-        which checks memory usage by the current python instance [every] ms
-        is [maxMemory] limit is reached raise MemoryError
         
-        this this nerving system freezes can be avoided
+        This function starts a daemon thread
+        which checks memory usage by the current Python instance [every] seconds
+        if [maxMemory] limit is reached it stops Python
+        ###
+        with this nerving system freezes can be avoided
         '''
         if maxMemory is None:
             mem = virtual_memory()
             maxMemory =  0.5 * mem.total/1024**3
         self.maxMemory = maxMemory
         self.every = every
-
 
     def __call__(self, func):
         stopFlag = Event()
@@ -53,14 +50,13 @@ class limitMemory(object):
         stopFlag.set()     
 
 
-    
+
 if __name__ == '__main__':
     import numpy as np
     from time import sleep
     l = []
 
-
-    @limitMemory(every=0.1, maxMemory=1)
+    @limitMemory(every=0.1, maxMemory=1) #or just add @limitMemory() for using default options
     def fillMemory():
         n=0
         while n<5000:
@@ -68,5 +64,3 @@ if __name__ == '__main__':
             l.append(np.random.rand(1e6))
             sleep(0.05)
             n+=1
-
-
